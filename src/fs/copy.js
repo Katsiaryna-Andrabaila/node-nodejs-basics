66a33de68e63d82c6e54276b880ac9e9ba37eec1
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-06-03 21:34:35 +0300

    feat: implement copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,40 @@
+import { access, F_OK, readdir, copyFile, mkdir } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const path = fileURLToPath(import.meta.url);
+const folder = join(dirname(path), "files");
+const newFolder = join(dirname(path), "files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  access(folder, F_OK, (e) => {
+    if (e) {
+      throw Error("FS operation failed");
+    } else {
+      access(newFolder, F_OK, (err) => {
+        if (err) {
+          mkdir(newFolder, (error) => {
+            if (error) throw error;
+            copyFolder();
+          });
+        } else {
+          throw Error("FS operation failed");
+        }
+      });
+    }
+  });
 };
+
+function copyFolder() {
+  readdir(folder, (e, files) => {
+    if (e) throw error;
+    for (const file of files) {
+      copyFile(join(folder, file), join(newFolder, file), (error) => {
+        if (error) throw error;
+      });
+    }
+  });
+}
 
 await copy();
