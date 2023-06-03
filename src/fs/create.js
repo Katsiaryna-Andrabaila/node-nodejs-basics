bc9ce8c09d5feddde714067bc28cda46a333833f
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-06-03 21:08:39 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { writeFile, access, F_OK } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(dirname(__filename), "files");
+const file = join(__dirname, "fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+  access(file, F_OK, (e) => {
+    if (e) {
+      writeFile(
+        file,
+        "I am fresh and young",
+        (err) => err && console.error(err)
+      );
+    } else {
+      throw Error("FS operation failed");
+    }
+  });
 };
 
 await create();
