a3cdf197127e077ffeff36c0f42a864e38955d3c
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-06-05 15:58:54 +0300

    feat: implement remove

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { access, F_OK, unlink } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const path = fileURLToPath(import.meta.url);
+const folder = join(dirname(path), "files");
+const file = join(folder, "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  access(file, F_OK, (e) => {
+    if (e) {
+      throw Error("FS operation failed");
+    } else {
+      unlink(file, (error) => error && console.error(error));
+    }
+  });
 };
 
 await remove();
