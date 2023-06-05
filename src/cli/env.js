d68b5ae8ed9d5696e8b04c81cc811f57114a8b86
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-06-05 16:44:34 +0300

    feat: implement env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,6 @@
 const parseEnv = () => {
-    // Write your code here 
+  const keys = Object.keys(process.env).filter((item) => item.includes("RSS_"));
+  keys.forEach((el) => console.log(`${el}=${process.env[el]}`));
 };
 
 parseEnv();
