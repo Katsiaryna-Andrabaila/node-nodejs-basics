6281f0aef3c64349c484f6c252d00c2dee6bc356
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-06-05 21:20:29 +0300

    feat: implement calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from "node:crypto";
+import { readFile } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const path = fileURLToPath(import.meta.url);
+const folder = join(dirname(path), "files");
+const file = join(folder, "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  readFile(file, "utf8", (error, content) => {
+    error && console.error(error);
+    const hash = createHash("sha256").update(content).digest("hex");
+    console.log(hash);
+  });
 };
 
 await calculateHash();
