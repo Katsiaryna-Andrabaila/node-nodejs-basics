60034cf544f9a2af4d665ead691fe277d7e8e2b6
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-06-05 16:11:35 +0300

    feat: implement list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { access, F_OK, readdir } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const path = fileURLToPath(import.meta.url);
+const folder = join(dirname(path), "files");
+
 const list = async () => {
-    // Write your code here 
+  access(folder, F_OK, (e) => {
+    if (e) {
+      throw Error("FS operation failed");
+    } else {
+      readdir(folder, (error, files) => {
+        error && console.error(error);
+        console.log(files);
+      });
+    }
+  });
 };
 
 await list();
