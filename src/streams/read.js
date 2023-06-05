fce92fb538244dad70c57c63b3d3ea0c550fd5aa
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-06-05 21:37:38 +0300

    feat: implement streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const { stdout } = process;
+
+const path = fileURLToPath(import.meta.url);
+const folder = join(dirname(path), "files");
+const file = join(folder, "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const myStream = createReadStream(file, "utf-8");
+  myStream.on("data", (chunk) => stdout.write(chunk));
 };
 
 await read();
