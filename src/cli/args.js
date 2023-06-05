d3d5b5178f45d068d65057572d3a50513f6ab7aa
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-06-05 17:04:24 +0300

    feat: implement args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const array = [];
+
+  const args = process.argv;
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i].slice(0, 2) === "--") {
+      array.push(`${args[i].slice(2)} is ${args[i + 1]}`);
+    }
+  }
+  console.log(array.join(", "));
 };
 
 parseArgs();
