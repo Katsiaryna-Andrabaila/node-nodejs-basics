bb63abb5a7da12226cf7730302b62dff29c26e31
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-06-05 16:16:11 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { access, F_OK, readFile } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const path = fileURLToPath(import.meta.url);
+const folder = join(dirname(path), "files");
+const file = join(folder, "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  access(file, F_OK, (e) => {
+    if (e) {
+      throw Error("FS operation failed");
+    } else {
+      readFile(file, "utf8", (error, content) => {
+        error && console.error(error);
+        console.log(content);
+      });
+    }
+  });
 };
 
 await read();
