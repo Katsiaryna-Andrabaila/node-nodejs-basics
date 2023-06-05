2ed806d63d8132a0a93bf692ad42ae124cabe33f
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-06-05 15:54:16 +0300

    feat: implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
-const rename = async () => {
-    // Write your code here 
+import { access, F_OK, rename } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const path = fileURLToPath(import.meta.url);
+const folder = join(dirname(path), "files");
+const file = join(folder, "wrongFilename.txt");
+const newFile = join(folder, "properFilename.txt");
+
+const renameFunc = async () => {
+  access(newFile, F_OK, (e) => {
+    if (e) {
+      access(file, F_OK, (err) => {
+        if (err) {
+          throw Error("FS operation failed");
+        } else {
+          rename(file, newFile, (error) => error && console.error(error));
+        }
+      });
+    } else {
+      throw Error("FS operation failed");
+    }
+  });
 };
 
-await rename();
+await renameFunc();
